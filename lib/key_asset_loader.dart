8600d4631f571a9dc4f73a000907345faedf7f34
Alejo <agiubel@gmail.com>
2023-07-11 11:36:01 -0400

    chore

---
+++ lib/key_asset_loader.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:http/http.dart' as http;
+
+class KeyAssetLoader extends AssetLoader {
+  @override
+  Future<Map<String, dynamic>> load(String path, Locale locale) async {
+    log('easy localization loader: load http $path');
+    log(locale.toString());
+
+    try {
+      var url = Uri.parse(path);
+      final Map<String, dynamic> result = await http
+          .get(url)
+          .then((response) => json.decode(response.body.toString()));
+      print(result);
+      return result[locale.languageCode];
+    } catch (e) {
+      //Catch network exceptions
+      return Future.value();
+    }
+  }
+}
